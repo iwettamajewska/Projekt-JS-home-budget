572e8c216d9a7cac8cfad87d5d4848c717231f25
iwetta <iwetta.majewska@gmail.com>
2022-08-23 16:29:35 +0200

    zmiany

---
+++ index.js
@@ -0,0 +1,35 @@
+let nameIncome = docoment.querySelector('#name-input');
+let amountCost = document.querySelector('#amount-input');
+let sumIncomeExpense = document.querySelector('#add-btn');
+let restSum = docoment.querySelector("#rest-sum");
+let unorderedListin = docoment.querySelector("#un-list-in");
+let unorderedListexp = docoment.querySelector("#un-list-exp");
+let incomeForm = docoment.querySelector("#income-form");
+const income = [];
+
+incomeForm.addEventListener('submit', function (event) {
+event.preventDefault();
+let addItems = {name: nameIncome.value, amount: amountCost.value, id: Math.random()};
+income.push(addItems);
+
+
+function addIncomeList() {
+    divy texty
+pętla 
+    
+income.forEach((item) => {
+    .......
+})
+};
+
+
+});
+
+
+
+
+
+
+
+
+
