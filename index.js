978761cbd305eb20ab548f1d8231942f44a0e69d
iwetta <iwetta.majewska@gmail.com>
2022-08-23 17:29:58 +0200

    zmiany

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-let nameIncome = docoment.querySelector('#name-input');
-let amountCost = document.querySelector('#amount-input');
-let sumIncomeExpense = document.querySelector('#add-btn');
-let restSum = docoment.querySelector("#rest-sum");
-let unorderedListin = docoment.querySelector("#un-list-in");
-let unorderedListexp = docoment.querySelector("#un-list-exp");
-let incomeForm = docoment.querySelector("#income-form");
+let nameIncome = document.querySelector("#name-input");
+let amountCost = document.querySelector("#amount-input");
+let sumIncomeExpense = document.querySelector("#add-btn");
+let restSum = document.querySelector("#rest-sum");
+let unorderedListin = document.querySelector("#un-list-in");
+let unorderedListexp = document.querySelector("#un-list-exp");
+let incomeForm = document.querySelector("#income-form");
 const income = [];
 
-incomeForm.addEventListener('submit', function (event) {
-event.preventDefault();
-let addItems = {name: nameIncome.value, amount: amountCost.value, id: Math.random()};
-income.push(addItems);
+incomeForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let addItems = {
+    name: nameIncome.value,
+    amount: amountCost.value,
+    id: Math.random(),
+  };
+  income.push(addItems);
 
-
-function addIncomeList() {
-    divy texty
-pętla 
-    
-income.forEach((item) => {
-    .......
-})
-};
-
-
+  addIncomeList();
+  
 });
 
+function addIncomeList() {
+    // divy texty
+    unorderedListin.innerText = "";
 
+    income.forEach((item) => {
+      const li = document.createElement("li");
+      const editButton = document.createElement("button");
+      const delateButton = document.createElement("button");
+      editButton.textContent = "Edytuj";
+      delateButton.textContent = "Usuń";
+      const span = document.createElement("span");
+      span.textContent = `${item.name} : ${item.amount} zł`;
+      li.appendChild(span);
+      li.appendChild(editButton);
+      li.appendChild(delateButton);
 
-
-
-
-
-
-
+      unorderedListin.appendChild(li);
+    });
+  }
