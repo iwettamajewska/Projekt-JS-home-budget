d46ba03749eb98902f2a6f57a9b0c39e10d3c573
iwetta <iwetta.majewska@gmail.com>
2022-08-27 09:30:18 +0200

    zmiany

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ let unorderedListexp = document.querySelector("#un-list-exp");
 let incomeForm = document.querySelector("#income-form");
 const income = [];
 
+// let editList = document.querySelector('')
+
+
 incomeForm.addEventListener("submit", function (event) {
   event.preventDefault();
   let addItems = {
@@ -16,26 +19,85 @@ incomeForm.addEventListener("submit", function (event) {
   };
   income.push(addItems);
 
-  addIncomeList();
+  addIncomeList(); //<= dlaczego ta linijka? nie wystarczyłoby tylko to co na dole w 22 linijce, czyli function addIncomeList() ??
   
 });
 
 function addIncomeList() {
     // divy texty
-    unorderedListin.innerText = "";
+    unorderedListin.innerText = ""; //dlaczego to się czyści na początku?
 
     income.forEach((item) => {
       const li = document.createElement("li");
       const editButton = document.createElement("button");
-      const delateButton = document.createElement("button");
+      const deleteButton = document.createElement("button");
       editButton.textContent = "Edytuj";
-      delateButton.textContent = "Usuń";
+      editButton.classList.add('edit-btn', 'li-btn');
+      deleteButton.textContent = "Usuń";
+      deleteButton.classList.add('delete-btn', 'li-btn');
       const span = document.createElement("span");
       span.textContent = `${item.name} : ${item.amount} zł`;
+      span.classList.add('text-in-list');
       li.appendChild(span);
       li.appendChild(editButton);
-      li.appendChild(delateButton);
+      li.appendChild(deleteButton);
 
       unorderedListin.appendChild(li);
+
+
+
+
     });
   }
+
+
+
+  
+
+editButton.addEventListener('click', function() {
+    let editButton = document.querySelector("edit-btn");
+    editButton.innerHTML = <button>Zapisz</button>;
+
+       
+       
+    })
+
+  
+
+
+
+
+
+  //   let saveButton = document.querySelector(".edit-btn");
+
+
+
+  //   document.addEventListener("DOMContentLoaded", function() {
+  //     let editButton = document.querySelector(".edit-btn");
+
+  //     myBtn.addEventListener("click", function() {
+  //       editButton.classList.add("save-btn");
+  //     })
+  // });
+
+
+
+
+
+
+// editButton.addEventListener('click', functiom () {
+// let zzzdsgdg = document.querySelector('wallet')
+
+//   });
+ 
+
+
+// Dlaczego li dodają się w poziomie a nie pionie?
+
+
+// do czego się odnosi? nie ma zmiennych i klas.
+// dlaczego math.random?
+//   name: nameIncome.value,
+//   amount: amountCost.value,
+//   id: Math.random(),
+  
