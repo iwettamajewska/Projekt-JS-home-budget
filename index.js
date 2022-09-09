4793b4ad5b8684bc5d8b4103b0c9438f702b9254
iwetta <iwetta.majewska@gmail.com>
2022-09-09 23:56:00 +0200

    zmiany - buttony(js) i css

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ incomeForm.addEventListener("submit", function(event) {
 });
 
 function addIncomeList() {
-    // divy texty
     unorderedListin.innerText = ""; //dlaczego to się czyści na początku?
 
     income.forEach((item) => {
       const li = document.createElement("li");
+      li.classList.add('li-income');
       const editButton = document.createElement("button");
       const deleteButton = document.createElement("button");
       editButton.textContent = "Edytuj";
@@ -39,8 +39,27 @@ function addIncomeList() {
       span.textContent = `${item.name} : ${item.amount} zł`;
       span.classList.add('text-in-list');
       li.appendChild(span);
-      li.appendChild(editButton);
-      li.appendChild(deleteButton);
+      // li.appendChild(editButton);
+      // li.appendChild(deleteButton);
+
+      // NOWE 45-61, zakomentowalam 42-43. Po tych kodach znika mi bullet point:
+      const boxForButtons = document.createElement("span");
+      boxForButtons.classList.add("boxButtons");
+      li.appendChild(boxForButtons);
+      boxForButtons.appendChild(editButton);
+      boxForButtons.appendChild(deleteButton);
+      const boxForSpan = document.createElement("span")
+      boxForSpan.classList.add('boxSpan');
+      li.appendChild(boxForSpan);
+      const saveButton = document.createElement('button');
+      const cancelButton = document.createElement('button');
+      saveButton.textContent = "Zapisz";
+      saveButton.classList.add('save-btn', 'li-btn', 'display-none');
+      cancelButton.textContent = "Anuluj";
+      cancelButton.classList.add('cancel-btn', 'li-btn', 'display-none');
+      boxForButtons.appendChild(saveButton);
+      boxForButtons.appendChild(cancelButton);
+
 
       unorderedListin.appendChild(li);
 
@@ -49,36 +68,38 @@ function addIncomeList() {
 }) 
 
 }
+      // NOWE:
+      editButton.addEventListener('click', function() {
+      editButton.remove();
+      editButton.classList.add('display-none');
+      deleteButton.classList.add('display-none');
+      saveButton.classList.remove('display-none');
+      canceleButton.classList.remove('display-none');
+   });
 
 
 
 
-  //     editButton.addEventListener('click', function() {
-  //     editButton.innerHTML = '<button class="save-button">Zapisz</button>';
-  //
-      
-
-    function createModal() {
-        const modal = document.createElement('span');
-        modal.classList.add('span-editable-modal');
-        const modalInput = document.createElement('input');
-        modalInput.classList.add('input-editable-modal');
-        const modalText = event.target.textContent;
-        modalInput.value = modalText;
-        const saveButton = document.createElement('button');
-        saveButton.classList.add('save-btn')
-        saveButton.textContent = 'Zapisz';
-        modal.appendChild(modalInput);
-        modal.appendChild(saveButton);
-        return modal;
-      }
+    // function createModal() {
+    //     modal.classList.add('span-editable-modal');
+    //     const modalInput = document.createElement('input');
+    //     modalInput.classList.add('input-editable-modal');
+    //     const modalText = event.target.textContent;
+    //     modalInput.value = modalText;
+    //     const saveButton = document.createElement('button');
+    //     saveButton.classList.add('save-btn')
+    //     saveButton.textContent = 'Zapisz';
+    //     modal.appendChild(modalInput);
+    //     modal.appendChild(saveButton);
+    //     return modal;
+    //   }
 
        
 
-      function editTask(event) {
-        const modal = createModal();
-        event.li.appendChild(modal);
-      }
+    //   function editTask(event) {
+    //     const modal = createModal();
+    //     event.li.appendChild(modal);
+    //   }
 
       
 
