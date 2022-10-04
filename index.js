a4c9084f219e9ebdc2962071e0d68ce2f1097989
iwetta <iwetta.majewska@gmail.com>
2022-10-04 23:48:33 +0200

    zmiany

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-let nameIncome = document.querySelector(".name-input");
-let amountCost = document.querySelector(".amount-input");
-let sumIncomeExpense = document.querySelector(".add-btn");
+let nameIncome = document.querySelector("#name-input");
+let amountCost = document.querySelector("#amount-input");
 let restSum = document.querySelector("#rest-sum");
-let unorderedListin = document.querySelector("#un-list-in");
-let unorderedListexp = document.querySelector("#un-list-exp");
+let unorderedListIn = document.querySelector("#un-list-in");
+let unorderedListExp = document.querySelector("#un-list-exp");
 let incomeForm = document.querySelector("#income-form");
+let expenseForm = document.querySelector("#expense-form");
+
 let income = [];
 let expense = [];
 
-
-
-
-incomeForm.addEventListener("submit", function(event) {
+incomeForm.addEventListener("submit", function (event) {
   event.preventDefault();
   let addItems = {
     name: nameIncome.value,
@@ -19,132 +17,161 @@ incomeForm.addEventListener("submit", function(event) {
     id: Math.random(),
   };
   income.push(addItems);
-  expense.push(addItems)
 
-  addIncomeList();
-  
+  addList("income");
 });
 
-function addIncomeList() {
-    unorderedListin.innerText = "";
+expenseForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let addItems = {
+    name: nameIncome.value,
+    amount: amountCost.value,
+    id: Math.random(),
+  };
+  expense.push(addItems);
 
-    income.forEach((item) => {
-      const li = document.createElement("li");
-      li.classList.add('li-income');
-      const editButton = document.createElement("button");
-      const deleteButton = document.createElement("button");
-      editButton.textContent = "Edytuj";
-      editButton.classList.add('edit-btn', 'li-btn', 'btn', 'btn-outline-light');
-      deleteButton.textContent = "Usuń";
-      deleteButton.classList.add('delete-btn', 'li-btn', 'btn', 'btn-outline-light');
-      let itemName = document.createElement('span');
-      itemName.classList.add('item-name');
-      let itemNameValue = document.createElement('span');
-      itemNameValue.classList.add('item-name-value');
-      itemName.textContent = `${item.name}`;
-      itemNameValue.textContent = `${item.amount} `;
-      let currency = document.createElement('span');
-      currency.classList.add('currency');
-      currency.textContent = `zł`;
-      li.appendChild(itemName);
-      li.appendChild(itemNameValue);
-      li.appendChild(currency);
+  addList("expense");
+});
 
-      const boxForButtons = document.createElement("span");
-      boxForButtons.classList.add("boxButtons");
-      li.appendChild(boxForButtons);
-      boxForButtons.appendChild(editButton);
-      boxForButtons.appendChild(deleteButton);
-      const boxForSpan = document.createElement("span")
-      boxForSpan.classList.add('boxSpan');
-      li.appendChild(boxForSpan);
-      const saveButton = document.createElement('button');
-      const cancelButton = document.createElement('button');
-      saveButton.textContent = "Zapisz";
-      saveButton.classList.add('save-btn', 'li-btn', 'display-none', 'btn', 'btn-outline-light');
-      cancelButton.textContent = "Anuluj";
-      cancelButton.classList.add('cancel-btn', 'li-btn', 'display-none', 'btn', 'btn-outline-light');
-      boxForButtons.appendChild(saveButton);
-      boxForButtons.appendChild(cancelButton);
-      calculateSum();
+function addList(type) {
+  const list = type === "income" ? unorderedListIn : unorderedListExp;
 
-      unorderedListin.appendChild(li);
-      const itemNameAmount = itemNameValue.textContent;
-      const itemNameVal = itemName.textContent;
+  list.innerText = "";
 
-      editButton.addEventListener("click", function () {
-        editButton.classList.add("display-none");
-        deleteButton.classList.add("display-none");
-        saveButton.classList.remove("display-none");
-        cancelButton.classList.remove("display-none");
-        itemName.setAttribute('contenteditable', 'true');
-        itemNameValue.setAttribute('contenteditable', 'true');
-        calculateSum();
-      });
-  
+  // type === "income"
+  //   ? (unorderedListIn.innerText = "")
+  //   : (unorderedListExp.innerText = "");
 
-      saveButton.addEventListener("click", function () {
-        editButton.classList.remove("display-none");
-        deleteButton.classList.remove("display-none");
-        saveButton.classList.add("display-none");
-        cancelButton.classList.add("display-none");
-        itemName.setAttribute('contenteditable', 'false');
-        itemNameValue.setAttribute('contenteditable', 'false');
-        income = income.map((element) => element.id === item.id ? {...element, name: itemName.textContent, amount: itemNameValue.textContent} : element);
-        calculateSum();
-      });
+  const source = type === "income" ? income : expense;
 
-  
-      cancelButton.addEventListener("click", function () {
-        
-        editButton.classList.remove("display-none");
-        deleteButton.classList.remove("display-none");
-        saveButton.classList.add("display-none");
-        cancelButton.classList.add("display-none");
-        itemName.setAttribute('contenteditable', 'false');
-        itemNameValue.setAttribute('contenteditable', 'false');
-        itemName.textContent = itemNameVal;
-        itemNameValue.textContent = itemNameAmount;
-      });
-  
-      deleteButton.addEventListener("click", 
-        (event) =>
-        deleteItem(item, event)
-      );
+  source.forEach((item) => {
+    const li = document.createElement("li");
+    li.classList.add("li-income");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    editButton.textContent = "Edytuj";
+    editButton.classList.add("edit-btn", "li-btn", "btn", "btn-outline-light");
+    deleteButton.textContent = "Usuń";
+    deleteButton.classList.add(
+      "delete-btn",
+      "li-btn",
+      "btn",
+      "btn-outline-light"
+    );
+    let itemName = document.createElement("span");
+    itemName.classList.add("item-name");
+    let itemNameValue = document.createElement("span");
+    itemNameValue.classList.add("item-name-value");
+    itemName.textContent = `${item.name}`;
+    itemNameValue.textContent = `${item.amount} `;
+    let currency = document.createElement("span");
+    currency.classList.add("currency");
+    currency.textContent = `zł`;
+    li.appendChild(itemName);
+    li.appendChild(itemNameValue);
+    li.appendChild(currency);
 
-      // let deleteButtonOnClick = document.createAttribute("onclick");
-      // deleteButtonOnClick.value = "deleteItem(this)";
-      // deleteButton.setAttributeNode(deleteButtonOnClick);
-    });
-  }
-  
-
-  function deleteItem(item, event) {
-  //  item.parentNode.parentNode.remove();
-    event.preventDefault();
-    console.log(item);
-    income = income.filter((element) => element.id !== item.id);
-    addIncomeList();
+    const boxForButtons = document.createElement("span");
+    boxForButtons.classList.add("boxButtons");
+    li.appendChild(boxForButtons);
+    boxForButtons.appendChild(editButton);
+    boxForButtons.appendChild(deleteButton);
+    const boxForSpan = document.createElement("span");
+    boxForSpan.classList.add("boxSpan");
+    li.appendChild(boxForSpan);
+    const saveButton = document.createElement("button");
+    const cancelButton = document.createElement("button");
+    saveButton.textContent = "Zapisz";
+    saveButton.classList.add(
+      "save-btn",
+      "li-btn",
+      "display-none",
+      "btn",
+      "btn-outline-light"
+    );
+    cancelButton.textContent = "Anuluj";
+    cancelButton.classList.add(
+      "cancel-btn",
+      "li-btn",
+      "display-none",
+      "btn",
+      "btn-outline-light"
+    );
+    boxForButtons.appendChild(saveButton);
+    boxForButtons.appendChild(cancelButton);
     calculateSum();
-  }
 
-  function editItem(item) {
-  
-  }
+    list.appendChild(li);
+    const itemNameAmount = itemNameValue.textContent;
+    const itemNameVal = itemName.textContent;
 
+    editButton.addEventListener("click", function () {
+      editButton.classList.add("display-none");
+      deleteButton.classList.add("display-none");
+      saveButton.classList.remove("display-none");
+      cancelButton.classList.remove("display-none");
+      itemName.setAttribute("contenteditable", "true");
+      itemNameValue.setAttribute("contenteditable", "true");
+      calculateSum();
+    });
 
-  
- 
-  function calculateSum() {
+    saveButton.addEventListener("click", function () {
+      editButton.classList.remove("display-none");
+      deleteButton.classList.remove("display-none");
+      saveButton.classList.add("display-none");
+      cancelButton.classList.add("display-none");
+      itemName.setAttribute("contenteditable", "false");
+      itemNameValue.setAttribute("contenteditable", "false");
+      const newArr = source.map((element) =>
+        element.id === item.id
+          ? {
+              ...element,
+              name: itemName.textContent,
+              amount: itemNameValue.textContent,
+            }
+          : element
+      );
+      type === "income" ? (income = newArr) : (expense = newArr);
+      calculateSum();
+    });
+
+    cancelButton.addEventListener("click", function () {
+      editButton.classList.remove("display-none");
+      deleteButton.classList.remove("display-none");
+      saveButton.classList.add("display-none");
+      cancelButton.classList.add("display-none");
+      itemName.setAttribute("contenteditable", "false");
+      itemNameValue.setAttribute("contenteditable", "false");
+      itemName.textContent = itemNameVal;
+      itemNameValue.textContent = itemNameAmount;
+    });
+
+    deleteButton.addEventListener("click", (event) => deleteItem(item, event));
+
+    // let deleteButtonOnClick = document.createAttribute("onclick");
+    // deleteButtonOnClick.value = "deleteItem(this)";
+    // deleteButton.setAttributeNode(deleteButtonOnClick);
+  });
+}
+
+function deleteItem(item, event) {
+  //  item.parentNode.parentNode.remove();
+  event.preventDefault();
+  console.log(item);
+  income = income.filter((element) => element.id !== item.id);
+  addList();
+  calculateSum();
+}
+
+function editItem(item) {}
+
+function calculateSum() {
   let incomeSum = income.reduce((acc, item) => {
-  return Number(acc) + Number(item.amount);
+    return Number(acc) + Number(item.amount);
   }, 0);
-  document.getElementById("sum-income").innerHTML =  incomeSum;
-  }
+  document.getElementById("sum-income").innerHTML = incomeSum;
+}
 calculateSum();
-
-
-
 
 // te wartosci sa 'na niby' - to taki mój szkic na następne kroki:
 
@@ -167,31 +194,20 @@ calculateSum();
 //    “Bilans jest ujemny. Jesteś na minusie XXX złotych”.
 //  }
 //  document.getElementById("rest-sum").innerHTML =  incomeSum;
-// } 
+// }
 
+/// zrob sobie z tego notatkę:
 
-
-
-
-
-
-
-   /// zrob sobie z tego notatkę:
-      
-    // function createModal() {
-    //     modal.classList.add('span-editable-modal');
-    //     const modalInput = document.createElement('input');
-    //     modalInput.classList.add('input-editable-modal');
-    //     const modalText = event.target.textContent;
-    //     modalInput.value = modalText;
-    //     const saveButton = document.createElement('button');
-    //     saveButton.classList.add('save-btn')
-    //     saveButton.textContent = 'Zapisz';
-    //     modal.appendChild(modalInput);
-    //     modal.appendChild(saveButton);
-    //     return modal;
-    //   }
-
-       
-
-
+// function createModal() {
+//     modal.classList.add('span-editable-modal');
+//     const modalInput = document.createElement('input');
+//     modalInput.classList.add('input-editable-modal');
+//     const modalText = event.target.textContent;
+//     modalInput.value = modalText;
+//     const saveButton = document.createElement('button');
+//     saveButton.classList.add('save-btn')
+//     saveButton.textContent = 'Zapisz';
+//     modal.appendChild(modalInput);
+//     modal.appendChild(saveButton);
+//     return modal;
+//   }
